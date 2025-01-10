6e75162ff263601048353d1b6d7b29ca5abeb6fb
Ilai Shoshani <ilaish27@gmail.com>
2025-01-10 11:52:16 +0200

    Added development cards but with no strategy and no road building logic

--- bot.py
+++ bot.py
@@ -19,10 +19,11 @@ class PRICES:
 class MyBot(CatanBot):
     fixed_land_value = [16, 16, 16, 16, 16, 0]
     virtual_land_value = [16, 16, 16, 16, 16, 0]
-    divide_when_taken = [2, 2, 2, 2, 2, 2]
+    divide_when_taken = [0.7, 0.7, 0.7, 0.7, 0.7, 0.7]
 
     land_num_to_score = {2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 8: 5, 9: 4, 10: 3, 11: 2, 12: 1}
     min_count_to_trade = 6
+    development_card_chance = 3
 
     def set_fixed_land_value_to_virtual(self):
         self.fixed_land_value = self.virtual_land_value
@@ -36,13 +37,14 @@ class MyBot(CatanBot):
             return 0
         land_score = self.land_num_to_score[land_num]
         value = self.virtual_land_value[land.value] * land_score
-        self.virtual_land_value[land.value] /= self.divide_when_taken[land.value]
+        # self.virtual_land_value[land.value] *= self.divide_when_taken[land.value]
         return value
 
     def rank_intersection(self, position):
         terrains = self.context.get_adjacent_terrains(position)
         self.virtual_land_value = self.fixed_land_value[::]
         res = sum(self.rank_land(land_pos) for land_pos in terrains)
+        # self.context.log_info("rank_intersection " + str(position) + str(res))
         self.set_fixed_land_value_to_virtual()
         return res
 
@@ -59,7 +61,28 @@ class MyBot(CatanBot):
             return
         elif self.trade_with_bank():
             return
+        elif self.try_build_development_cards():
+            return
         return
+
+    def try_build_development_cards(self):
+        if random.randint(0, self.development_card_chance):
+            if self.context.buy_development_card() == Exceptions.OK:
+                return True
+        return False
+
+    def before_dice(self):
+        dev_cards:  DevelopmentCardCounts = self.context.get_development_cards()
+        if dev_cards[DevelopmentCards.YEAR_OF_PLENTY] > 0:
+            requested_counts: ResourceCounts = ResourceCounts()
+            requested_counts[self.most_needed_resource()] = 2  # Request all.
+            self.context.play_year_of_plenty(requested_counts)
+        elif dev_cards[DevelopmentCards.MONOPOLY] > 0:
+            self.context.play_monopoly(self.most_needed_resource())
+        elif dev_cards[DevelopmentCards.ROAD_BUILDING] > 0:
+            self.context.play_road_building(self.)  # TODO: fix
+        elif dev_cards[DevelopmentCards.KNIGHT] > 0:
+            self.context.play_knight()
 
     def build_city(self):
         buildings = self.context.get_player_buildings(self.context.get_player_index())
@@ -81,8 +104,6 @@ class MyBot(CatanBot):
 
         return False
 
-
-
     def build_road(self):
         edges= self.context.get_edges()
         random.shuffle(edges)
@@ -96,12 +117,16 @@ class MyBot(CatanBot):
                 pass
         return
 
+    def most_needed_resource(self):
+        res_counts = self.context.get_resource_counts()
+        return Resources(res_counts.index(min(res_counts)))
+
     def trade_with_bank(self):
         res_counts = self.context.get_resource_counts()
-        min_resource = res_counts.index(min(res_counts))
+        min_resource = self.most_needed_resource()
         for i, count in enumerate(res_counts):
             if count >= self.min_count_to_trade:
-                self.context.maritime_trade(res_counts[i], Resources(min_resource))
+                self.context.maritime_trade(min_resource, Resources(min_resource))
                 return True
         return False
 
