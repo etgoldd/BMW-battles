06484b0d99e8ac981940bfa66d65fa8e0bbf3a28
Ethan <etgold05@gmail.com>
2025-01-10 10:06:49 +0200

    added prices list

--- bot.py
+++ bot.py
@@ -1,6 +1,13 @@
 from api import *
 
 
+class PRICES:
+    ROAD = ResourceCounts(lumber=1, brick=1),
+    SETTLEMENT = ResourceCounts(lumber=1, brick=1, grain=1, wool=1)
+    CITY = ResourceCounts(wool=2, ore=3)
+    DEVELOPMENT_CARD = ResourceCounts(grain=1, wool=1, ore=1)
+
+
 class MyBot(CatanBot):
     
     def setup(self):
@@ -9,5 +16,9 @@ class MyBot(CatanBot):
     def play(self):
         pass
 
-    def place_settlements_and_roads(self):
+    def place_settlement_and_road(self):
         pass
+
+    def drop_resources(self):
+        resources = 
+        self.context.set_resources_to_drop()
