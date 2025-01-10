08250d21c7e07930b2960a3227bfc20bd5c1bbcb
omrisdeor <omrisdeor@mail.tau.ac.il>
2025-01-10 10:07:34 +0200

    added play skeleton

--- bot.py
+++ bot.py
@@ -7,7 +7,21 @@ class MyBot(CatanBot):
         pass
 
     def play(self):
-        pass
+        cards = self.context.get_resource_counts()
+        lumber, brick, grain, wool, ore = cards
+        if can_build_city:
+            build_city
+            return
+        elif can_place_settlement:
+            build_best_settlement
+            return
+        elif can_place_road:
+            build_road
+            return
+        elif trade_with_bank:
+            return
+
+            
 
     def place_settlements_and_roads(self):
         pass
